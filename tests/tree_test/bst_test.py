7981e38da2f1488eeefdbe83c3b133e1957d4c49
Albert Wu <albertwujj@gmail.com>
2019-01-18 10:41:52 -0800

    added rb bst from level-order

--- tests/tree_test/bst_test.py
+++ tests/tree_test/bst_test.py
@@ -3,6 +3,7 @@ from trees.bst import BST
 from trees.bst import Node
 import random
 import math
+from queue import Queue
 
 def black_height(curr):
     if curr is None:
@@ -21,6 +22,30 @@ def consec_red(curr):
     curr_red = curr._Node__red
     return curr_red, (two_red_l or two_red_g) or (curr_red and (one_red_l or one_red_g))
 
+def rb_bst_from_level_order(level_ord):
+    """
+    :return:
+    """
+    level_ord = level_ord.split()
+    val, color = level_ord[0].split('.')
+    level_ord[0] = Node(val, _Node__isred=color=='R')
+
+    for i in range(len(level_ord)):
+        curr = level_ord[i]
+        i = i + 1
+        if curr is not None and type(curr) != str:
+            li = (i * 2) - 1
+            ri = (i * 2)
+            if li < len(level_ord) and level_ord[li] != 'null':
+                print(i, li)
+                left_val, left_color = level_ord[li].split('.')
+                curr.setleft(Node(left_val, _Node__isred=left_color=="R"))
+                level_ord[li] = curr.left
+            if ri < len(level_ord)  and level_ord[ri] != 'null':
+                right_val, right_color = level_ord[ri].split('.')
+                curr.setright(Node(right_val, _Node__isred=right_color=="R"))
+                level_ord[ri] = curr.right
+    return level_ord[0]
 
 
 class TestRedBlack(unittest.TestCase):
@@ -35,9 +60,6 @@ class TestRedBlack(unittest.TestCase):
             for _ in range(n):
                 bst.insert(random.randint(0,1000))
             self.insert_trees.append(bst)
-
-
-
 
 
 
@@ -58,7 +80,15 @@ class TestRedBlack(unittest.TestCase):
                 bh_rule = False
                 break
         self.assertTrue(bh_rule)
-
+    """
+    def test_black_heights_deletion(self):
+        bh_rule = True
+        trees = self.delete_trees
+        for bst in trees:
+            if black_height(bst.root) == -1:
+                bh_rule = False
+        self.assertTrue(bh_rule)
+    """
     def test_consec_reds_insertion(self):
         red_rule = True
         trees = self.insert_trees
@@ -69,6 +99,7 @@ class TestRedBlack(unittest.TestCase):
                 break
         self.assertTrue(red_rule)
 
+
     def test_consec_red_fail(self):
         """
         # violate tree's red property
@@ -76,15 +107,18 @@ class TestRedBlack(unittest.TestCase):
         consec_red_tree.insert_no_rebalance(random.randint(0,1000))
         self.assertTrue(consec_red(consec_red_tree.root))
         """
-    """
-    def test_black_heights_deletion(self):
-        bh_rule = True
-        trees = self.delete_trees
-        for bst in trees:
-            if black_height(bst.root) == -1:
-                bh_rule = False
-        self.assertTrue(bh_rule)
-    """
+
+    def test_consec_red_fail_succeed(self):
+
+    def test_consec_red_fail_explicit(self):
+        red_rule = True
+        explicit_trees = ['1.B 2.B 3.B null 5.R 6.B null null null 6.B']
+        for strtree in explicit_trees:
+            curr = rb_bst_from_level_order(strtree)
+            _, has_consec_red = red_rule(curr)
+            if has_consec_red:
+                red_rule = False
+        self.assertFalse(red_rule)
 
 def suite():
     suite = unittest.TestSuite()
